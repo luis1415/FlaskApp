06449adae7545f651d22110740b06041a4486e4c
luis1415 <luisbermudeztorres@gmail.com>
2017-06-26 18:30:58 -0500

    Register

--- app.py
+++ app.py
@@ -2,18 +2,27 @@ from flask import Flask, render_template, flash, redirect, url_for, session, log
 from data import articles
 from wtforms.fields.html5 import EmailField
 from flask_mysqldb import MySQL
-from wtforms import StringField, TextAreaField, PasswordField, validators
-from flask_wtf import FlaskForm
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
-app.secret_key = 'llavesecreta123'
+# app.secret_key = 'llavesecreta123'
 # Se debe llamar la funcion aqui, y se guarda en una variable
 Articles = articles()
 
+# configuracion de MySQL
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask_example'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Inicializar MySQL
+mysql = MySQL(app)
+
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('home.html')
 
 
@@ -32,14 +41,14 @@ def article(id_):
     return render_template('article.html', id=id_)
 
 
-class MyForm(FlaskForm):
+class MyForm(Form):
     name = StringField('Nombre', validators=[validators.data_required(message="Nombre es un campo requerido")])
     username = StringField('Username', validators=[validators.Length(min=1, max=50)])
     email = EmailField('E-mail', validators=[validators.Length(min=6, max=50)])
     password = PasswordField('Password', validators=[validators.Length(min=1, max=50),
-                                                   validators.DataRequired(message='Debe ingresar un password'),
-                                                   validators.equal_to('confirm', message='passwords no coinciden')
-                                                   ])
+                                                     validators.DataRequired(message='Debe ingresar un password'),
+                                                     validators.equal_to('confirm', message='passwords no coinciden')
+                                                     ])
     confirm = PasswordField('Confirmar Password')
 
 
@@ -47,8 +56,28 @@ class MyForm(FlaskForm):
 def register():
     form = MyForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('register.html')
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # se crea el cursor
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "INSERT INTO users(name, email, username, password) VALUES (%s, %s, %s, %s)", (name, email, username,
+                                                                                           password))
+        # commit a la base de datos
+        mysql.connection.commit()
+
+        # cerrar el cursor
+        cur.close()
+
+        # mensajes en flask se usa flash, para eso se agrega en includes los mensajes
+        flash('Registrado', 'success')
 
     return render_template('register.html', form=form)
+
+
 if __name__ == '__main__':
+    app.secret_key = 'llavesecreta1234'
     app.run(debug=True)
